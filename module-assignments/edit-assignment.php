986e98d5c594ae8c7a6a9a4de5eb37d9813eb342
EncikTajul <tajulafiq0@gmail.com>
2025-12-20 03:49:28 +0800

    chore: Remove old Ez2Learn files, replaced by new system in New Ez2Learn folder

--- module-assignments/edit-assignment.php
+++
@@ -1,5 +0,0 @@
-<?php
-// This file redirects to create-assignment.php with the ID parameter
-// The create-assignment.php handles both create and edit functionality
-header('Location: create-assignment.php?id=' . ($_GET['id'] ?? ''));
-exit();
